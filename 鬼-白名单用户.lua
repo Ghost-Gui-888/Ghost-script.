a517d21da40c469a5e552da410f0b0b8aa064a27
Ghost nb <3065395172@qq.com>
2025-01-02 21:11:22 +0800

    Update V2

---
+++ 鬼-白名单用户.lua
@@ -0,0 +1,15 @@
+return {
+["KS_G66"] = true,
+["CN_BLPO"] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+[""] = true,
+}
+
